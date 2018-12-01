575332460c4e08ea570c4a6b42533f93858ed0d8
dcy <18964580031@163.com>
2018-12-01 12:06:33 +0800

    整理完毕

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /**
- * author: stephentian
- * email: stephentian@foxmail.com
- * day: 2018-10-22
+ * author: Dingcy
+ * day: 2018-12-1
  **/
 
 // 这个项目是为了帮助开发者掌握 JavaScript 概念而创立的。它不是必备，但在未来学习（JavaScript）中，可以作为一篇指南。
